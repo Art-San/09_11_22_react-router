107a2f175192f005992e56ba5ed8121e15490c80
Aleksander <asava@sibmail.com>
2022-11-09 18:23:28 +0700

    end

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,9 @@
 import NavBar from "./components/NavBar";
+import { Route } from 'react-router-dom'
+import Dashboard from "./components/Dashboard"
+import Posts from './components/Posts'
+import Login from './components/Login'
+import Home from './components/Home'
 
 
 function App() {
@@ -6,6 +11,10 @@ function App() {
     <div>
       <NavBar/>
       <h1>App</h1>
+      <Route path='/dashboard' component={Dashboard}/>
+      <Route path='/posts' component={Posts}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/' component={Home}/>
     </div>
   );
 }
