60f837f43c20c95a27cfc5a0f6975b85b515cdf0
Aleksander <asava@sibmail.com>
2022-11-10 14:33:56 +0700

    obed

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,20 @@
 import NavBar from "./components/NavBar";
 import { Route, Switch } from 'react-router-dom'
 import Dashboard from "./components/Dashboard"
-import Posts from './components/Posts'
+// import Posts from './components/Posts'
 import Login from './components/Login'
 import Home from './components/Home'
 import Stats from "./components/Stats";
-//
+import PostsList from "./components/PostsList";
+import Post from "./components/Post";
+
+const posts = [
+  {id: 1, label: 'post 1'},
+  {id: 2, label: 'post 2'},
+  {id: 3, label: 'post 3'},
+
+]
+
 function App() {
   return (
     <div>
@@ -14,16 +23,17 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/dashboard/stats' component={Stats}/>
-        <Route 
-            path='/dashboard'
-            render={(props) => {
-                return (
-                  false && <Dashboard isAdmin={false} {...props}/>
-                )
-            }}
-        />
-        <Route path='/posts' component={Posts}/>
+        <Route path='/dashboard' component={Dashboard}/>
         <Route path='/login' component={Login}/>
+        <Route 
+            path='/posts/:postId'
+            render={(props) => <Post pasts={posts} {...props}/>}
+          />
+        <Route 
+            path='/posts'
+            render={(props) => <PostsList pasts={posts} {...props}/>}
+          />
+        
       </Switch>
     </div>
   );
