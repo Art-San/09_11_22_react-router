8209f0344d66a4ee749207b7e6ef2603cdc2423e
Aleksander <asava@sibmail.com>
2022-11-11 00:05:07 +0700

    end

--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import PostsList from "./PostsList";
 import Post from "./Post";
 import query from 'query-string'
-import _ from 'lodash'
+// Перейдём на адрес “/posts?fromUrl=https://example.partner.com&block=pr_1”
 
-const Posts = ({match, location}) => {
+const Posts = ({ match, location }) => {
+  const [showOffer, setShowOffer] = useState(false);
+
   const posts = [
-    {id: 1, label: 'post 1'},
-    {id: 2, label: 'post 2'},
-    {id: 3, label: 'post 3'},
-  ]
+      { id: 1, title: "Post 1" },
+      { id: 2, title: "Post 2" },
+      { id: 3, title: "Post 3" }
+  ];
 
-  // const string = [
-  //   {ifso: 'car', label: 'Машина'},
-  //   {ifso: 'bar', label: 'Бар без пива'},
-  //   {ifso: 'dar', label: 'Дар такойкакой есть'},
-  // ]
-  
-  const search = query.parse(location.search)
-  const postId = match.params.postId
-  // console.log('search', search.ifso)
-  const cropPosts = search ? _(posts).slice(0).take(search.count).value() : posts
-  // const cropPosts = string.filter((e, i) => e.ifso === search.ifso)
-  // console.log('cropPosts', cropPosts)
+  const search = query.parse(location.search);
+  const { fromUrl, block } = search;
+  console.log('fromUrl, block', fromUrl, block)
+
+  useEffect(() => {
+      if (!showOffer && fromUrl && block) {
+          setShowOffer(true);
+      }
+  }, [fromUrl, block, showOffer]);
+
+  const postId = match.params.postId;
+  const getPost = (postId) => posts.find(({ id }) => String(id) === postId);
+
   return (
-    <>
-      {postId ? (
-          <Post posts={posts} id={postId}/>
-      ) : (
-          <PostsList posts={cropPosts}/>
-      )}
-    </>
-  )
-}
+      <>
+          {showOffer && <div>Специальное предложение</div>}
+          {postId ? (
+              <Post post={getPost(postId)} id={postId} />
+          ) : (
+              <PostsList posts={posts} />
+          )}
+      </>
+  );
+};
 
-export default Posts
+export default Posts;
