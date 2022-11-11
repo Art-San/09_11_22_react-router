55b0cbf10ad8f3cc4793ecca218dae484c1fec5d
Aleksander <asava@sibmail.com>
2022-11-11 17:06:39 +0700

    end 11.11.22

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import NavBar from "./components/NavBar";
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Dashboard from "./components/Dashboard"
 import Posts from './components/Posts'
 import Login from './components/Login'
 import Home from './components/Home'
 import Stats from "./components/Stats";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -16,10 +17,31 @@ function App() {
         <Route path='/dashboard/stats' component={Stats}/>
         <Route path='/dashboard' component={Dashboard}/>
         <Route path='/login' component={Login}/>
-        <Route path='/posts/:postId?/:display?' component={Posts}/> 
+        <Route path='/posts/:postId?/:display?' component={Posts}/>
+        <Route path='/404'component={NotFound} />
+        <Redirect from="/admin" to='/dashboard' />
+        <Redirect to='/404'/>
       </Switch>
     </div>
   );
 }
 
-export default App;
+// Второй вариант
+// const App = () => {
+//   return (
+//       <div>
+//           <NavBar />
+//           <h1>App</h1>
+//           <Switch>
+//               <Route path="/" exact component={Home} />
+//               <Route path="/signin" component={Login} />
+//               <Route path="/posts/:postId?" component={Posts} />
+//               <Route path='/dashboard/stats' component={Stats}/>
+//               <Route path="/dashboard" component={Dashboard} />
+//               <Redirect from="/lk" to="/signin" />
+//               <Route component={NotFound} />
+//           </Switch>
+//       </div>
+//   )
+// }
+export default App
--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -11,18 +11,10 @@ const Posts = ({match, location}) => {
     {id: 3, label: 'post 3'},
   ]
 
-  // const string = [
-  //   {ifso: 'car', label: 'Машина'},
-  //   {ifso: 'bar', label: 'Бар без пива'},
-  //   {ifso: 'dar', label: 'Дар такойкакой есть'},
-  // ]
-  
   const search = query.parse(location.search)
   const postId = match.params.postId
-  // console.log('search', search.ifso)
+ 
   const cropPosts = search ? _(posts).slice(0).take(search.count).value() : posts
-  // const cropPosts = string.filter((e, i) => e.ifso === search.ifso)
-  // console.log('cropPosts', cropPosts)
   return (
     <>
       {postId ? (
