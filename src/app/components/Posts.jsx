dc17a89406fa99029627231d71fec9765dc71c11
Aleksander <asava@sibmail.com>
2022-11-21 22:59:37 +0700

    end

--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import PostsList from "./PostsList";
+import qs from 'query-string'
 
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  const [showOffer, setShowOffer] = useState(false)
+  console.log('showOffer', showOffer)
     const posts = [
         { id: 1, title: "Post 1" },
         { id: 2, title: "Post 2" },
         { id: 3, title: "Post 3" }
     ];
+    // if (!showOffer && fromUrl && block)
+    const search = qs.parse(location.search)
+    const { fromUrl, block}= search
+    console.log('block',  fromUrl, '||', block)
 
-    // Получение URL параметра
+    useEffect(() => {
+      if (!showOffer && fromUrl && block) {
+          setShowOffer(true);
+      }
+  }, [fromUrl, block, showOffer]);
+
     const postId = match.params.postId;
-
-    // Логика получения записи перенесена из Post
     const getPost = (postId) => posts.find(({ id }) => String(id) === postId);
 
-    // Если есть postId, то возвращаем Post, иначе PostList
-    return postId ? (
+    return (
+      <>
+      {showOffer && <div>Специальное предложение</div>}
+      {postId ? (
         <Post post={getPost(postId)} id={postId} />
     ) : (
         <PostsList posts={posts} />
-    );
+    )}
+      </>
+    )
 };
 
 export default Posts;
