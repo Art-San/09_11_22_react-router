f1f18e933e96a7a3c8b99d8c3279e95e811d9ca2
Aleksander <asava@sibmail.com>
2022-11-21 23:34:31 +0700

    Forwarding (redirect)

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SignIn from "./components/SignIn";
 import Contacts from "./components/Contacts";
 import Posts from "./components/Posts";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
         <Route path="/signin" component={SignIn} />
         <Route path="/posts/:postId?" component={Posts} />
         <Route path="/contacts" component={Contacts} />
+        <Route path='/404' component={NotFound}/>
+        <Redirect from="/lk" to="/signin" />
+        <Redirect to='/404'/>
       </Switch>
     </div>
   );
