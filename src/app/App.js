a400751c6a855b4961b38385fc0e96a8d082afbe
Aleksander <asava@sibmail.com>
2022-11-21 20:17:29 +0700

    end

--- src/app/App.js
+++ src/app/App.js
@@ -3,15 +3,8 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SignIn from "./components/SignIn";
-import PostsList from "./components/PostsList";
 import Contacts from "./components/Contacts";
-import Post from "./components/Post";
-
-const posts = [
-  { id: 1, title: "Post 1" },
-  { id: 2, title: "Post 2" },
-  { id: 3, title: "Post 3" }
-];
+import Posts from "./components/Posts";
 
 function App() {
   return (
@@ -21,12 +14,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signin" component={SignIn} />
-        <Route path="/posts/:postId">
-                    {(props) => <Post {...props} posts={posts} />}
-                </Route>
-        <Route path="/posts">
-                    {(props) => <PostsList {...props} posts={posts} />}
-                </Route>
+        <Route path="/posts/:postId?" component={Posts} />
         <Route path="/contacts" component={Contacts} />
       </Switch>
     </div>
--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-const Post = ({ match, posts }) => {
-    const postId = match.params.postId;
-
-    // В хранилище записей id - число, 
-    // а значения в match.params всегда строки
-    const getPost = (postId) => posts.find(({ id }) => String(id) === postId);
-    const post = getPost(postId);
-
-    // Выведем заголовок если есть запись и сообщение об ошибке если нет
-    return (
-        <h1>{post ? post.title : `post with id: ${postId} is not found`}</h1>
-    );
+const Post = ({ post, id }) => {
+    return <h1>{post ? post.title : `post with id: ${id} is not found`}</h1>;
 };
+
+// const Post = ({ match, posts }) => {
+//     const postId = match.params.postId;
+
+//     // В хранилище записей id - число, 
+//     // а значения в match.params всегда строки
+//     const getPost = (postId) => posts.find(({ id }) => String(id) === postId);
+//     const post = getPost(postId);
+
+//     // Выведем заголовок если есть запись и сообщение об ошибке если нет
+//     return (
+//         <h1>{post ? post.title : `post with id: ${postId} is not found`}</h1>
+//     );
+// };
 
 export default Post;
