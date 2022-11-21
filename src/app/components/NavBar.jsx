c0f28a13550519e4246341ff203186ebf974bb24
Aleksander <asava@sibmail.com>
2022-11-21 12:16:52 +0700

    end

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -5,16 +5,16 @@ const NavBar = () => {
   return (
     <ul>
         <li>
-            <a href="/">Home</a>
+            <a href="/">Главная</a>
         </li>
         <li>
-            <a href="/login">Login</a>
+            <a href="/login">Вход в кабинет</a>
         </li>
         <li>
-            <a href="/posts">Posts</a>
+            <a href="/posts">Записи</a>
         </li>
         <li>
-            <a href="/dashboard">Dashboard</a>
+            <a href="/dashboard">Контакты</a>
         </li>
     </ul>
   )
