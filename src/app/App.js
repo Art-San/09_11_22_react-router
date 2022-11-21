f7ca5dd09ab513ded608651109af713ea2ca1203
Aleksander <asava@sibmail.com>
2022-11-21 13:36:26 +0700

    end

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,24 @@
-import { Route } from "react-router-dom";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SignIn from "./components/SignIn";
 import Posts from "./components/Posts";
 import Contacts from "./components/Contacts";
-
-
-
+import SomePost from "./components/SomePost";
 
 function App() {
   return (
     <div>
       <NavBar/>
       <h1>App</h1>
-      <Route path="/" exact component={Home} />
-      <Route path="/signin" component={SignIn} />
-      <Route path="/posts" component={Posts} />
-      <Route path="/contacts" component={Contacts} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/posts/some-post" component={SomePost} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/contacts" component={Contacts} />
+      </Switch>
     </div>
   );
 }
---
+++ src/app/components/SomePost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SomePost = () => {
+  return (
+    <button className='btn btn-primary'>SomePost</button>
+  )
+}
+
+export default SomePost
